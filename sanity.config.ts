bbbdedfe23ee26285d16bf4b3ff8858a726b8263
Lakata80 <endarovlazar@gmail.com>
2026-06-19 16:40:02 +0300

    Sanity addition

---
+++ sanity.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import schema from './sanity/schema'
+
+export default defineConfig({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset:   process.env.NEXT_PUBLIC_SANITY_DATASET ?? 'production',
+  title:     'MeeplesBG CMS',
+  plugins:   [structureTool()],
+  schema:    { types: schema },
+})
