f5235bb00268049b292c5b6ad5b5095e70b41489
Lakata80 <endarovlazar@gmail.com>
2026-06-28 20:36:33 +0300

    Sentry fixes

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -6,14 +6,6 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://a145c78d04483231855a1ea71175cc89@o4511644168093696.ingest.de.sentry.io/4511644178120784",
-
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
-
-  // Enable logs to be sent to Sentry
   enableLogs: true,
-
-  // Enable sending user PII (Personally Identifiable Information)
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
 });
