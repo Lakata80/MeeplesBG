ad6bf7e95beadb2894b0a476670f10b2783b1632
Lakata80 <endarovlazar@gmail.com>
2026-06-30 20:13:27 +0300

    Fix for page.tsx

--- app/(site)/novini/[slug]/page.tsx
+++ app/(site)/novini/[slug]/page.tsx
@@ -15,7 +15,7 @@ import ShareButtons          from '@/components/blog/ShareButtons'
 import BlogCommentsSection   from '@/components/blog/BlogCommentsSection'
 import PostCard              from '@/components/blog/PostCard'
 
-export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 // ── Статично генериране ───────────────────────────────────────
 
